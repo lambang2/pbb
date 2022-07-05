98aba54e19b832210e15781b302c7602cb6e9f3f
lambang2 <93249848+lambang2@users.noreply.github.com>
2022-07-05 11:56:40 +0700

    Add files via upload

---
+++ pwl-uas/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Off Day Jacket</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Off Day Jacket</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>Off Day Jacket - Moh. Lambang Prayugo - <a href="" target="_blank">A11.2020.80009</a></p>
+  </div>
+
+</body>
+
+</html>
